da24c2a5db80596c6c100ad2ebcab82d88f4eacb
Stéphane Gully <stephane.gully@gmail.com>
2014-07-30 18:02:58 +0200

    first commit

---
+++ test/t1.js
@@ -0,0 +1,36 @@
+var LogIoServerParser = require('../lib/logio-server-parser.js');
+
+var lines = '+log|my_stream|my_node|info|this is log message\r\n' +
+  '+node|my_node\r\n' +
+  '+node|my_node|my_stream1,my_stream2\r\n' +
+  '-node|my_node\r\n' +
+  'line not well formatted\r\n';
+
+
+//var request = require('request');
+
+var nodes = {};
+
+
+//logStream.pipe(request.post(options)).pipe(resultStream);
+
+
+var server = new LogIoServerParser({ port: 28777 });
+
+server.on('+node', function (node, streams) {
+  console.log('+node', node, streams);
+});
+
+server.on('-node', function (node) {
+  console.log('-node', node);
+});
+
+server.on('+log', function (stream, node, type, log) {
+  console.log('-log', stream, node, type, log);
+});
+
+server.on('unknown', function (data) {
+  console.log('unknown', data);
+});
+
+server.parseLines(lines);
